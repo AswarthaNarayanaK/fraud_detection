14c3e90b567be1078d2cc714ba0ad5e1b0a99d2c
AswarthaNarayanaK <aswarth.ece@gmail.com>
2025-04-25 17:08:43 +0530

    added three new models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("adding all ML related code here ")
+
+def model1():
+    print("This is model 1")
+    # Add your model 1 code here   
+def model2():
+    print("This is model 2")
+    # Add your model 2 code here
+def model3():
+    print("This is model 3")
+    # Add your model 3 code here
